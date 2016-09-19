6cb8743473ba7594e8ee73ec519f0af1800123cb
pimpakarn.w <itao.huhu@gmail.com>
2016-09-19 16:00:15 +0700

    test

--- app/src/main/java/com/example/pimpakarnw/jsonexample/MainActivity.java
+++ app/src/main/java/com/example/pimpakarnw/jsonexample/MainActivity.java
@@ -55,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         {
             e.printStackTrace();
         }
+
+        /*Test*/
+        System.out.println("TAO");
     }
 
     /*@Override
